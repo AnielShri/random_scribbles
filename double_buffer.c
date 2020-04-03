3f396623129b850a3d0c213fdda88f550807deb9
Aniel Shri <36446127+AnielShri@users.noreply.github.com>
2020-04-03 13:56:34 +0200

    Create double_buffer.c

---
+++ double_buffer.c
@@ -0,0 +1,36 @@
+// ----------------------------------------------------------------------------+
+//
+//	Double Buffer example
+//
+//	This example shows how to use the DMA Half-Complete-Callback and the
+//	Complete-Callback to avoid simultaneously reading from and writing to
+//	a certain buffer.
+//
+//	When the Half-Complete ISR is called, data from PART1 can safely be read
+//	while PART2 is being written to. When the Complete ISR is called, data from
+//	PART 2 is read, while the DMA writes to PART 1.
+//
+//	                       |                     |
+//	+---------------------+|+-------------------+|
+//	|        PART1        |||      PART2        ||
+//	+---------------------+|+-------------------+|
+//	                       |                     |
+//	                       HALF-COMPLETE         COMPLETE   
+//
+// ----------------------------------------------------------------------------+
+
+#include <stdio.h>	// memcpy
+
+#define BUFFER 100
+uint32_t adc_input[2*BUFFER];
+uint32_t dac_output[2*BUFFER]
+
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	memcpy(dac_output, adc_input, FILTER_BUFFER*sizeof(adc_input[0]));
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	memcpy(dac_output+FILTER_BUFFER, adc_input+FILTER_BUFFER, FILTER_BUFFER*sizeof(adc_input[0]));
+}
